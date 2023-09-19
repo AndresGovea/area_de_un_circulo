d2c0af331fe798e540b20abdae56975e8d9ac44f
Andrés <143667914+AndresGovea@users.noreply.github.com>
2023-09-18 21:00:18 -0600

    Versión 2

---
+++ area de un rectangulo.cpp
@@ -0,0 +1,19 @@
+// area de un cuadrado.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+
+void main(void)
+{
+	int base;
+     int altura;
+      int area;
+    printf("Base:");
+        scanf_s("%i", &base);
+    prinft("Altura:");
+        scanf_s("%i", &altura);
+    area = base * altura;
+
+    printf("El área del rectangulo es: %i", area);
+}
+ 
