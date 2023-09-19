6339cfb579a47ddbaf0c461ee8e64f1ca7e4d6b1
Andrés <143667914+AndresGovea@users.noreply.github.com>
2023-09-18 21:05:37 -0600

    Versión 3

---
+++ area de un circulo.cpp
@@ -0,0 +1,18 @@
+// area de un cuadrado.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+
+void main(void)
+{
+	int radio;
+     float pi;
+      float area;
+    printf("Radio:");
+        scanf_s("%i", &radio);
+    pi: 3.141592653;
+    area = pi * radio * radio;
+
+    printf("El área del círculo es: %i", area);
+}
+ 
